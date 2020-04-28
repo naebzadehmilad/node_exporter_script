40d79123dc1548e7ac40818fc7d09e7d5ede4e00
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-04-28 16:10:35 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.6
 import os
 from  jinja2 import Template
 from config import *
@@ -29,8 +30,12 @@ def node_exporter():
     f.close()
     print(len(hosts))
     for i in range(len(hosts)):
+        os.system("ssh  root@{0} 'useradd --no-create-home --shell /bin/false node_exporter'".format(hosts[i])) 
+        os.system("ssh  root@{0} 'wget {1} && tar -xvf node_exporter-* && '".format(hosts[i],link ))
+        os.system("ssh root@{0} ' rm -rf node_exporter-*.tar.gz* &&  mv node_exporter-* node_exporter '".format(hosts[i]))
+        os.system("ssh root@{0} 'cp -r node_exporter/node_exporter /usr/local/bin/'".format(hosts[i]))
+        print(hosts[i],':>>>')
         os.system(('scp -o ConnectTimeout=3 tmp/node_exporter.service root@{0}:/etc/systemd/system/').format(hosts[i])) == 0
-        if exit(0):
-            print('please go to server {0} and run systemctl enable node_exporter && systemctl start node_exporter'.format(hosts[i])) == 0
+        print('please go to server {0} and run systemctl enable node_exporter && systemctl start node_exporter'.format(hosts[i])) == 0
 
 node_exporter()
