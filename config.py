552cc1b6fb879834d9b0929b0db2c30d0506bbba
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-04-28 16:11:09 +0430

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,18 @@ def write_conf():
     conf.set('hosts','nodes','127.0.0.1,127.0.0.2')
     conf.add_section('port')
     conf.set('port','port','9100')
+    conf.add_section('download')
+    conf.set('download','link','https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz')
     with open('conf','w') as configfile:
         conf.write(configfile)
 def read_conf():
       conf.read('conf')
       global hosts
       global port
+      global  link
       hosts = str(conf.get('hosts', 'nodes')).replace(' ', '').replace(' ', '[').replace(' ',']').split(',')
       port = str(conf.get('port', 'port')).replace(' ', '').replace(' ','[')
+      link = str(conf.get('download', 'link')).replace(' ', '').replace(' ', '[').replace(' ',']')
       logging.info('  create conf \n')
 if os.path.exists('conf') :
     read_conf()
